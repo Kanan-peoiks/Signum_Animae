d8244cf02a0ed7c0c136dea165df154979a01557
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-24 20:32:02 +0400

    first commit

---
+++ src/main/java/com/example/signum_animae/SignumAnimaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.signum_animae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignumAnimaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignumAnimaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/signum_animae/SignumAnimaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signum_animae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignumAnimaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
